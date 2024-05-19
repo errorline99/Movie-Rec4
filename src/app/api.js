0c3544333b265ab8ca2574aab34c4c4893a96753
errorline99 <100209216+errorline99@users.noreply.github.com>
2024-05-19 16:15:06 +0300

    Add files via upload

---
+++ src/app/api.js
@@ -0,0 +1,68 @@
+
+import Home from './page';
+// import '../Gone.css';
+import Uses from './components/uses';
+import RootLayout from './layout';
+import Client from './components/page'
+
+
+export function vv() {
+  var doc = document.querySelector('.image-container')
+  if(typeof vv.i === 'undefined') {
+    vv.i = 50
+  } 
+
+
+  vv.i+= 150;
+
+  if(vv.i >= 1750) {
+    vv.i = 0
+  }
+  doc.scrollTo({
+    top: 100,
+    left: vv.i,
+    behavior: "smooth",
+  })
+  
+
+
+
+}
+
+
+
+export default async function Gone(props) {
+
+  const items = props.image
+  const finish = props.code
+  const title = props.titles
+  const overviews = props.summa
+
+
+  return (
+    <div className="gone-container">
+      <div className='con-cov'>
+        <div className='ann-cov'>
+        <img className='img-cov loaded' src={items[0]}/>
+        <p className='img-text'>{overviews[0]}</p>
+        </div>
+      </div>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+   
+      <br/>
+      <div className="image-container">
+        {items.map((user, index) => <div key={index} className="image-wrapper">
+          <img className='try' id={'' +index} src={user} alt={""}/>
+          <p className="image-title"><br/></p>
+         </div>)}
+      </div>
+      <Client sums={overviews} tooth={title}/>
+    </div>
+    
+  );
+ 
+}
+
---
+++ src/app/components/switch.js
@@ -0,0 +1,12 @@
+"use client"
+import { useRef, useEffect } from 'react';
+
+
+
+export default function Pull(props) { 
+
+
+    return(<>
+
+    </>)
+}
